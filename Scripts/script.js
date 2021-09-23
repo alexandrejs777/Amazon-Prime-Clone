f76c5cf3b44e5b0c7a87b8af55f118b5c4cee4cf
alexandrejs777 <85634326+alexandrejs777@users.noreply.github.com>
2021-09-23 18:47:20 -0300

    End

---
+++ Scripts/script.js
@@ -0,0 +1,73 @@
+var main = document.querySelectorAll('.main .center')[0];
+
+fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=1')
+.then(response => response.json())
+.then(data => {
+    data.results.map((i, j) => {
+
+        // Catálogo de filmes
+        // > 18 é o número de filmes que vão aparecer no catálogo
+        // Original é o tamanho original da imagem, e o w500 é o dimensionamento do width em 500px
+        console.log(data)
+
+        if (j > 20){
+            return;
+        }
+        if (j == 0){
+            main.innerHTML +=`      
+            <div class="banner-principal" style="background-image: url('https://image.tmdb.org/t/p/original`+ i.backdrop_path +`')"></div>
+            `;
+        } else {
+            if (j == 1){
+                main.innerHTML += `
+
+                    <h2>Assista o melhor filme.</h2>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w400`+ i.poster_path +`" /></div>
+
+                `;
+            } else {
+                main.innerHTML += `
+
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w400`+ i.poster_path +`" /></div>
+
+                `;
+            } 
+        }
+    })
+})
+
+fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=2')
+.then(response => response.json())
+.then(data => {
+    data.results.map((i, j) => {
+
+        // Catálogo de filmes
+        // > 18 é o número de filmes que vão aparecer no catálogo
+        // Original é o tamanho original da imagem, e o w500 é o dimensionamento do width em 500px
+        console.log(data)
+
+        if (j > 20){
+            return;
+        }
+        if (j == 0){
+            main.innerHTML +=`      
+            <div class="banner-principal" style="background-image: url('https://image.tmdb.org/t/p/original`+ i.backdrop_path +`')"></div>
+            `;
+        } else {
+            if (j == 1){
+                main.innerHTML += `
+
+                    <h2>Assista o melhor filme.</h2>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w400`+ i.poster_path +`" /></div>
+
+                `;
+            } else {
+                main.innerHTML += `
+
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w400`+ i.poster_path +`" /></div>
+
+                `;
+            } 
+        }
+    })
+})
